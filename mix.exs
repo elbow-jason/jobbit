ddf3f9db22a8a85d15a60868eb0464246e24ab4b
Jason Goldberger <jlgoldb2@asu.edu>
2020-04-23 13:01:18 -0700

    updated and fixed mix.exs version and deps

--- mix.exs
+++ mix.exs
@@ -4,28 +4,19 @@ defmodule Jobbit.Mixfile do
   def project do
     [
       app: :jobbit,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
       package: package(),
       deps: deps(),
+      apps: apps()
     ]
   end
 
   defp apps do
-    [
-      :logger
-    ]
-  end
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [
-      applications: apps,
-    ]
+    []
   end
 
   defp deps do
